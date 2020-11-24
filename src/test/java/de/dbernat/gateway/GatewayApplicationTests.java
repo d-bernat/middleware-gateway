569944bcbc237a1718d9bbf35aa709a84c3c78a1
d-bernat <mail@it-bernat.de>
2020-11-24 23:21:19 +0100

    failed test

--- src/test/java/de/dbernat/gateway/GatewayApplicationTests.java
+++ src/test/java/de/dbernat/gateway/GatewayApplicationTests.java
@@ -1,13 +1,17 @@
 package de.dbernat.gateway;
 
+import org.assertj.core.api.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @SpringBootTest
 class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue("fail forever", false);
 	}
 
 }
