b1bb3850aff14ec8b80a31d044c0cb48c952b1b9
d-bernat <mail@it-bernat.de>
2020-11-25 08:54:16 +0100

    test ci notif

--- src/test/java/de/dbernat/gateway/controller/AliveControllerTest.java
+++ src/test/java/de/dbernat/gateway/controller/AliveControllerTest.java
@@ -1,8 +1,16 @@
 package de.dbernat.gateway.controller;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AliveControllerTest
 {
+    @Test
+    void justForCiTest(){
+        assertEquals(3,4);
+    }
 
 }
